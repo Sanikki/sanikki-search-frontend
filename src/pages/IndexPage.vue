bd962d1cb563629f92de8f31b324eff02bbf6cdb
QAQ <954677615@qq.com>
2024-05-09 14:37:13 +0800

    项目初始化

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="index-page">
+    <a-input-search
+      v-model:value="searchText"
+      placeholder="input search text"
+      enter-button="Search"
+      size="large"
+      @search="onSearch"
+    />
+    <a-tabs v-model:activeKey="activeKey">
+      <a-tab-pane key="1" tab="Tab 1">
+        <PostList />
+      </a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2" force-render
+        >Content of Tab Pane 2
+      </a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3">Content of Tab Pane 3</a-tab-pane>
+    </a-tabs>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import PostList from "@/components/PostList.vue";
+
+const searchText = ref("");
+const activeKey = ref("1");
+const onSearch = (searchValue: string) => {
+  console.log("use value", searchValue);
+  console.log("or use this.value", searchText.value);
+};
+</script>
